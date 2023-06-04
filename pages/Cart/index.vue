e989020bd61fdf38b49103c9f7de9379d3073e8d
Yodra Muhamad Akbar N <yodra.muhamad@gmail.com>
2023-06-04 18:25:18 +0700

    fix responsive

--- pages/Cart/index.vue
+++ pages/Cart/index.vue
@@ -90,8 +90,8 @@ const success = () =>{
         <Suspense>
             <template #default>
                 <div class="container mx-auto mt-10">
-                    <div class="flex shadow-md my-10">
-                    <div class="w-3/4 bg-white px-10 py-10">
+                    <div class="flex flex-col md:flex-row shadow-md my-10">
+                    <div class="w-full md:w-3/4 bg-white px-10 py-10">
                         <div class="flex justify-between border-b pb-8">
                         <h1 class="font-semibold text-2xl">Shopping Cart</h1>
                         <h2 class="font-semibold text-2xl">{{ data.length }} Items</h2>
@@ -136,7 +136,7 @@ const success = () =>{
                         </NuxtLink>
                     </div>
 
-                    <div id="summary" class="w-1/4 px-8 py-10 bg-neutral-100">
+                    <div id="summary" class="w-full md:w-1/4 px-8 py-10 bg-neutral-100">
                         <h1 class="font-semibold text-2xl border-b pb-8">Order Summary</h1>
                         <div class="flex justify-between mt-10 mb-5">
                         <span class="font-semibold text-sm uppercase">Items {{ data.length }}</span>
